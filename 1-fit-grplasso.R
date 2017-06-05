82f2f83a6ef2357aaec2b384d79fdb82de4db955
TimothyKBook <TimothyKBook@gmail.com>
2017-06-04 22:04:27 -0400

    Initial run done - wow!

---
+++ 1-fit-grplasso.R
@@ -0,0 +1,62 @@
+options(stringsAsFactors = FALSE)
+
+cwd <- '.'
+setwd(cwd)
+
+library(dplyr)
+library(grplasso)
+
+train <- read.csv('data/train.csv')
+test <- read.csv('data/test.csv')
+
+# Strip covariates by type
+covars <- train %>% select(starts_with('X'))
+cat_vars <- covars[sapply(covars, function(x) class(x) == 'character')]
+num_vars <- covars[sapply(covars, function(x) class(x) != 'character')]
+
+### Prepare categorical variables ==============================================
+# Produce design matrix of categorical variables
+cat_mm <- as.data.frame(model.matrix(~ . - 1, data = cat_vars))
+
+# Create grouped lasso indices
+cat_names <- colnames(cat_vars)
+for (cn in cat_names) {
+  assign(paste0(cn, '_ind'), which(grepl(cn, colnames(cat_mm))))
+}
+
+# Create integer indices for grplasso call
+p_cat <- ncol(cat_mm)
+grp_ind <- rep(0, p_cat)
+for (i in 1:length(cat_names)) {
+  cn <- cat_names[i]
+  grp_ind[get(paste0(cn, '_ind'))] <- i
+}
+
+### Prepare quantitative variables =============================================
+# Remove variables that have too-high purity.  ie, if they are too similar to
+# being all-zeros or all-ones.
+cut_tol <- 0.01
+bin_purity <- unname(sapply(num_vars, mean))
+num_keep <- (bin_purity < 1 - cut_tol) & (bin_purity > cut_tol)
+table(num_keep)
+
+num_vars <- num_vars[num_keep]
+
+### Combine variables, get params input-ready ==================================
+y <- train$y
+logy <- log(y)
+x <- cbind(cat_mm, num_vars)
+
+df <- data.frame(y, x)
+df_log <- data.frame(logy, x)
+
+grp_ind <- c(grp_ind, rep(NA, ncol(num_vars)))
+
+### Fit model ==================================================================
+m1 <- grplasso(formula = y ~ ., data = df, lambda = 1, model = LinReg())
+yhat <- fitted(m1)
+
+# Rsq of 61.4%, wow!
+sse <- sum((y - yhat)^2)
+ssto <- sum((y - mean(y))^2)
+1 - sse / ssto
